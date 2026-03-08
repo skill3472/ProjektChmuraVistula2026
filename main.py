dab1a4a075cd24090c1cd756f586b9ca6c16a7ab
Maksymilian Tym <social@mtym.me>
2026-03-08 13:25:56 +0100

    feat: Added support for local .env

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
+from contextlib import asynccontextmanager
+
 from azure.cosmos import CosmosClient
+from dotenv import load_dotenv
 from fastapi import Depends, FastAPI, Request
 from fastapi.templating import Jinja2Templates
 
 from src.database import get_cosmos_client
 from src.repository import DatabaseRepository
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Lifespan context manager."""
+    load_dotenv()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
 def get_root(request: Request):
     """Renders a simple HTML page with the current counter value."""
     return templates.TemplateResponse("index.html", {"request": request})
+
 
 @app.get("/counter")
 def read_counter(cosmos: CosmosClient = Depends(get_cosmos_client)):  # noqa: B008
@@ -19,6 +32,7 @@ def read_counter(cosmos: CosmosClient = Depends(get_cosmos_client)):  # noqa: B0
     repo = DatabaseRepository()
     count = repo.get_count_db(None, cosmos)
     return {"value": count}
+
 
 @app.post("/counter/increment")
 def increment_counter(cosmos: CosmosClient = Depends(get_cosmos_client)):  # noqa: B008
